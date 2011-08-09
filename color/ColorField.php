7c428e1bd6ceff4571369e7ae3e7edc691493de1
Christian Bachhelm <bachhelmc@p7media.de>
2011-08-09 21:47:37 +0300

    Fixed a typo to create set_source_file_location($s)

--- color/ColorField.php
+++ color/ColorField.php
@@ -4,7 +4,7 @@ class ColorField extends TextField {
 
 	protected static $source_file_location = "mysite/thirdparty/formfields/color/";
 		public static function get_source_file_location() {return self::$source_file_location;}
-		public static function get_source_file_location($s) {self::$source_file_location = $s;}
+		public static function set_source_file_location($s) {self::$source_file_location = $s;}
 
 	function __construct($name, $title = null, $value = '#000000') {
 		parent::__construct($name, $title, $value);
